4b7620661d67ca4b34f99602eba93069275c0059
Sayyed Mohammad Reza Dadkhahi <sm.dadkhahi@gmail.com>
2022-01-18 15:22:26 +0330

    add spring boot
    add empty spring boot to project

---
+++ src/main/java/com/startupsland/zarinpalspringboot/ZarinpalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.startupsland.zarinpalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarinpalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarinpalSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startupsland/zarinpalspringboot/ZarinpalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startupsland.zarinpalspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarinpalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
